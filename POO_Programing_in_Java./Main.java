e960647628bb6d5a12bc3a5c5709d540b172de73
carolinaasouza939-svg <carolinaasouza939@gmail.com>
2026-01-18 16:23:36 -0300

    Change LookAndFeel to CrossPlatform in Main.java

--- POO_Programing_in_Java./Main.java
+++ POO_Programing_in_Java./Main.java
@@ -6,7 +6,8 @@ import javax.swing.UIManager;
 public class Main {
     public static void main(String[] args){
         try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            // Define o visual do Sistema;
+            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
         } catch (Exception e) { 
             e.printStackTrace();
         }
